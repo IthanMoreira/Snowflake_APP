2bc28abfc4aa08f4b2c881a561f320fe56fbb69b
Ithan Moreira <ithan.moreira@gmail.com>
2022-06-22 10:39:38 -0400

    Update APP.py

--- APP.py
+++ APP.py
@@ -14,3 +14,18 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Mostramos el titulo
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Seleccion multiple de frutas
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
+
