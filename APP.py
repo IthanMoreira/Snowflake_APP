13551a8c284467f356ea33caa2c88b5b0c8ade7f
Ithan Moreira <ithan.moreira@gmail.com>
2022-06-22 10:33:55 -0400

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,3 +1,16 @@
 import streamlit
+import pandas
+import requests
 
-streamlit.title('Hola, Esto es un titulo')
+import snowflake.connector
+from urllib.error import URLError
+
+# Añadimos el titulo de nuestro sitio web
+
+streamlit.title("Listado de comida")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
